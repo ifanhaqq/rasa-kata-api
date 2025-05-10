de9e96fd75ce03b48913284281daa509fb1545d3
ifanhaq <haqinhanifan@gmail.com>
2025-05-11 00:46:33 +0700

    Refactor routes.js to include welcome message handler for root path

--- routes.js
+++ routes.js
@@ -3,12 +3,24 @@ const { getAllFeedbacksHandler, addFeedbackHandler } = require("./handler");
 module.exports = [
   {
     method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return h
+        .response({
+          status: "success",
+          message: "Welcome to the Rasa Kata API",
+        })
+        .code(200);
+    },
+  },
+  {
+    method: "GET",
     path: "/feedback",
-    handler: getAllFeedbacksHandler
+    handler: getAllFeedbacksHandler,
   },
   {
     method: "POST",
     path: "/feedback",
-    handler: addFeedbackHandler
+    handler: addFeedbackHandler,
   },
 ];
